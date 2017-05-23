460c40b7381b3cbdd75642a44a2e9898c1b0450a
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-05-23 15:01:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function getFirstSelector(selector){
+     return document.querySelector(selector)
+}
+function nestedTarget(){
+     const element = document.getElementById('nested')
+     return element
+}
+
+function increaseRankBy(){
+     
+}
